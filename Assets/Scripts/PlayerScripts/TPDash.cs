175980aff9af7835fb681de684c13774d40e7ed0
Wilson Vo <brawilson@hotmail.com>
2021-05-26 05:28:02 -0700

    WVo_IndependentProjectPhase1
    Created:
    - Character Model
    - Graybox
    -Platforms
    - Kill Box
    
    Player GameObject [Empty with model child]
    - Can move with WASD and Arrow Keys
    - Press Space to Jump [if held, jump a bit higher]
    - Left Click Mouse to Dash
    
    - Player Gravity/GroundCheck vs. GroundLayer
    
    Need to work on:
    - Dash feels weird. Not snappy.
    - Want dash to have cooldown
    - Low jumps to feel less floaty?
    
    - Dashes should give invulnerability against kill blocks.

---
+++ Assets/Scripts/PlayerScripts/TPDash.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPDash : MonoBehaviour
+{
+    ThirdPersonMovement moveScript;
+
+    public float dashSpeed;
+    public float dashTime;
+    public float dashCooldown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveScript = GetComponent<ThirdPersonMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            StartCoroutine(Dash());
+
+            gameObject.tag = "Player";
+            Debug.Log("Player");
+        }
+    }
+
+    IEnumerator Dash()
+    {
+        float startTime = Time.time;
+
+        gameObject.tag = "Invuln";
+        Debug.Log("Invulnerable");
+
+        while (Time.time < startTime + dashTime)
+        {
+            moveScript.controller.Move(moveScript.moveDir * dashSpeed * Time.deltaTime);
+
+            
+
+            yield return null;
+        }
+    }
+}
