2b2e6ccd1ab126e2bbc08fcc5ea637631d17f0dd
Wilson Vo <brawilson@hotmail.com>
2021-05-28 12:13:27 -0700

    WVo_IndependentProjectPhase3
    Requirements:
    1) Imported a free asset [I used Unit 3's Townspeople as a template since it also came with animations I understand]
    - Townspeople Animatoins modified
    
    2) gameOver bool created which SpawnManager, Player controls [not including camera], and other functions rely on in order to keep functioning.
    - Effectively stops all player movement and stops the spawnmanager from spawning objects.
    - Appropriate sound effects, particles, etc. during gameOver = true.
    
    3) Multi state animation controller included [Unit 3's modified]
    
    4) Animations and transitions to different animations all accessed/triggered via code.
    - When no movement --> idle animation
    - When moving --> running animation
    - Click for Dash --> "Fall" animation [located in "Death" Layer
    - Space Bar to Jump --> Run-Jump animation.
    
    5) Particle System included and triggered via code.
    - When Player is killed, the model is Destroyed followed by an exploding particle system [cyan-blue color]
    
    6) Audio added. Some triggered by code.
    - Background music [from Tap Tap Fish - Abyssrium]
    - Jump sound effect
    - Dash sound effect
    - "Boom" death sound effect
    
    # All sound effects are rips from Super Mario 3D World. All property of Nintendo.
    
    God this phase was awfully hard...
    
    For Phase 4:
    - Goal Post/Checkpoints
    - Level Designs
    - Title/Game Over Screen
    - UI [Deaths/Tries & Timer]
    
    - Maybe clean up some of the assets too. Some are scrapped now.

---
+++ Assets/_Scripts/PlayerScripts/JumpGravity.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Courtesy of Youtube Video "THIRD PERSON MOVEMENT in Unity" by Brackeys @ May 24, 2020
+// and "FIRST PERSON MOVEMENT in Unity - FPS Controller by Brackeys @ Oct 27, 2019
+// "HOLD JUMP KEY TO JUMP HIGHER - 2D PLATFORMER CONTROLLER - UNITY TUTORIAL by Blackthornprod @ Jul 9, 2018
+
+public class JumpGravity : MonoBehaviour
+{
+    private ThirdPersonMovement TPM;
+    private Animator animPlayer;
+
+    private float gravity = -18.36f;
+
+    public CharacterController controller;
+    public Transform groundCheck;
+
+
+    public float jumpHeight = 3f;
+    public float jumpTime = 0.6f;
+
+    private float jumpTimeCounter;
+
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+    bool isJumping;
+
+    public AudioClip jumpSound;
+    private AudioSource asPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TPM = GameObject.Find("Player").GetComponent<ThirdPersonMovement>();
+        animPlayer = GameObject.Find("Distorter").GetComponent<Animator>();
+
+        asPlayer = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TPM.gameOver == false)
+        {
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+            // Gravity
+            if (isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+
+
+            // Jump
+            if (isGrounded && Input.GetKey(KeyCode.Space))
+            {
+                // Play Jump Animation/Sound ONCE
+                if(isGrounded && Input.GetKeyDown(KeyCode.Space))
+                {
+                    animPlayer.SetTrigger("Jump_trig");
+                    asPlayer.PlayOneShot(jumpSound);
+                }
+
+                isJumping = true;
+                jumpTimeCounter = jumpTime;
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+
+            // Still holding Space while jumping = Higher Jump
+            if (Input.GetKey(KeyCode.Space) && isJumping)
+            {
+                if (jumpTimeCounter > 0)
+                {
+                    velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+                    jumpTimeCounter -= Time.deltaTime;
+                }
+                else
+                {
+                    isJumping = false;
+                }
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                isJumping = false;
+            }
+        }
+    }
+}
